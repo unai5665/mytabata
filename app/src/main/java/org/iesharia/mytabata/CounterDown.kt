04f220b2d0e68bf517b9d9c286738581d2d4b6ae
unai5665 <151141045+unai5665@users.noreply.github.com>
2024-10-21 18:31:33 +0100

    Actualizado CounterDown.kt, implementado pausar y reanudar

--- app/src/main/java/org/iesharia/mytabata/CounterDown.kt
+++ app/src/main/java/org/iesharia/mytabata/CounterDown.kt
@@ -2,41 +2,51 @@ package org.iesharia.mytabata
 
 import android.os.CountDownTimer
 import android.util.Log
-import androidx.compose.runtime.Composable
 
 class CounterDown(var segundos: Int, var loquehacealhacertick: (Long) -> Unit) {
-    private var counterState : Boolean = false
+    private var counterState: Boolean = false
+    private var currentTimer: CountDownTimer? = null
+    private var remainingTime: Long = segundos * 1000L
 
-    val myCounter = object : CountDownTimer((segundos * 1000L), 1000) {
+    private fun createTimer(timeInMillis: Long) {
+        currentTimer = object : CountDownTimer(timeInMillis, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                if (counterState) {
+                    remainingTime = millisUntilFinished
+                    loquehacealhacertick(millisUntilFinished / 1000)
+                }
+            }
 
-        override fun onTick(millisUntilFinished: Long) {
-            if (counterState) loquehacealhacertick(millisUntilFinished / 1000)
-        }
-
-        override fun onFinish() {
-            counterState = false
-            Log.i("dam2", "mensajito")
+            override fun onFinish() {
+                counterState = false
+                Log.i("dam2", "Timer finished")
+            }
         }
     }
 
     fun toggle() {
         Log.i("dam2", "toggle: $counterState")
-        if (this.counterState){
-            this.cancel()
-
+        if (counterState) {
+            pause()
         } else {
-            Log.i("dam2", "toggle: start")
-            this.start()
+            start()
         }
     }
 
     fun start() {
-        counterState = true
-        this.myCounter.start()
+        Log.i("dam2", "Starting timer")
+        if (!counterState) {
+            counterState = true
+            createTimer(remainingTime) // Recreate the timer with the remaining time
+            currentTimer?.start()
+        }
     }
 
-    fun cancel() {
-        counterState = false
-        this.myCounter.cancel()
+    fun pause() {
+        Log.i("dam2", "Pausing timer")
+        if (counterState) {
+            counterState = false
+            currentTimer?.cancel() // Stop the timer
+        }
     }
 }
